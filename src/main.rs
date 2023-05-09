dea1fc3e6bec6e94cd141e3658a95a1b92a9988b
Luke Swithenbank <lswith@users.noreply.github.com>
2023-05-09 12:51:09 +1000

    make it OOM while deploying

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,25 @@ use std::{
 };
 
 use axum::{extract::State, routing::get, Router};
+use tokio::task;
 
 #[tokio::main]
 async fn main() {
     let data: Arc<Mutex<Vec<u8>>> = Arc::new(Mutex::new(Vec::new()));
+
+    let data_clone = data.clone();
+
+    task::spawn(async move {
+        loop {
+            {
+                let mut newdata: Vec<u8> = vec![u8::MAX; 100 * MEGABYTES];
+                let mut data = data_clone.lock().unwrap();
+                data.append(&mut newdata);
+            }
+            println!("Adding more data to the vector");
+            tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+        }
+    });
 
     let app = Router::new()
         .route("/", get(handler))
@@ -28,7 +43,7 @@ const GIGABYTES: usize = 1024 * MEGABYTES;
 
 async fn handler(State(state): State<Arc<Mutex<Vec<u8>>>>) -> String {
     {
-        let mut newdata: Vec<u8> = vec![u8::MAX; 10 * MEGABYTES];
+        let mut newdata: Vec<u8> = vec![u8::MAX; 100 * MEGABYTES];
         let mut data = state.lock().unwrap();
         data.append(&mut newdata);
     }
